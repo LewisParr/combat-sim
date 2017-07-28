058543ce8a5724a42beb4fbfe8d57a7813fdadf5
Lewis C. Parr <lewisparr@gmail.com>
2017-07-28 13:52:28 +0100

    Timesteps, Asset Graphs, COA, and Asset Assignment
    Added timestep function, asset graphs, course of action development, and asset assignment.

--- simulation.py
+++ simulation.py
@@ -4,65 +4,178 @@
 import scenario
 import environment
 import commander
+import event
 
+import numpy as np
+import networkx as nx
 import matplotlib.pyplot as plt
+
+def timestep():
+    # Detection
+    for F in force:
+        F.detect(env, force[F.enemy_forceID])
+    # Give orders
+    for F in force:
+        F.giveOrders()
+    # Event creation
+    event_queue = []
+    for F in force:
+        [forceID, companyID, platoonID, sectionID, manID, eventType, eventData] = F.createEvents(env, force[F.enemy_forceID])
+        for a in np.arange(0, len(forceID)):
+            for b in np.arange(0, len(companyID[a])):
+                for c in np.arange(0, len(platoonID[a][b])):
+                    for d in np.arange(0, len(sectionID[a][b][c])):
+                        if eventType[a][b][c][d] == 'MOVE':
+                            source = [forceID[a], companyID[a][b], platoonID[a][b][c], sectionID[a][b][c][d], manID[a][b][c][d]]
+                            event_queue.append(event.MovementEvent(source, eventData[a][b][c][d]))
+    # Event effect calculation
+    # ...
+    # Event result application
+    for E in event_queue:
+        if E.type == 'MOVE':
+            E.apply(force)
 
 # Initialise environment
 print('Initialising environment...')
-env = environment.Environment()
+#env = environment.Environment(260, 260)
 print('Generating terrain...')
-env.genTerrain(250, 250, 2)
+#env.genTerrain(1)
+#env.saveElevationData()
+#env.loadElevationData()
 
 # Plot terrain
 print('Plotting terrain...')
-plt.figure()
-plt.contourf(env.X, env.Y, env.Z)
-plt.title('Elevation Contour Plot')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('Terrain Elevation')
-plt.colorbar()
-plt.show()
+env.plotElevation()
+
+# Build visibility maps
+print('Building visibility maps...')
+#env.buildVisibilityMaps()
+#env.saveVisibilityMaps()
+#env.loadVisibilityMaps()
 
 # Initialise scenario
 print('Initialising scenario...')
 scene = scenario.TestScenario()
 
-# Initialise enemy commander
-print('Initialising adversary forces...')
-adversary = commander.EnemyCommander()
-adversary.assignObjective(scene.adversary_objective)
-adversary.assignAssets(scene.adversary_assets)
-adversary.assignAssetLocations(scene.adversary_asset_locations)
+# Initialise commanders
+print('Initialising commanders...')
+force = []
+force.append(commander.EnemyCommander(0))
+force.append(commander.FriendlyCommander(1))
+force[0].assignObjective(scene.adversary_objective)
+force[0].assignAssets(scene.adversary_assets)
+force[0].assignAssetLocations(scene.adversary_asset_locations)
+force[1].assignObjective(scene.friendly_objective)
+force[1].assignAssets(scene.friendly_assets)
+force[1].assignAssetLocations(scene.friendly_asset_locations)
 
-# Initialise friendly commander
-print('Initialising friendly forces...')
-friendly = commander.FriendlyCommander()
-friendly.assignObjective(scene.friendly_objective)
-friendly.assignAssets(scene.friendly_assets)
-friendly.assignAssetLocations(scene.friendly_asset_locations)
+# Create graph of enemy assets
+E = nx.DiGraph()
+color_map = []
+E.add_node(force[0])
+color_map.append('red')
+for C in force[0].company:
+    E.add_node(C)
+    color_map.append('orange')
+    E.add_edge(force[0],C)
+    for P in C.platoon:
+        E.add_node(P)
+        color_map.append('yellow')
+        E.add_edge(C,P)
+        for S in P.section:
+            E.add_node(S)
+            color_map.append('green')
+            E.add_edge(P,S)
+
+# Draw graph of enemy assets
+plt.figure()
+plt.title('Enemy Assets')
+nx.draw(E, node_color=color_map)
+plt.show()
+
+# Create graph of friendly assets
+F = nx.DiGraph()
+color_map = []
+F.add_node(force[1])
+color_map.append('red')
+for C in force[1].company:
+    F.add_node(C)
+    color_map.append('orange')
+    F.add_edge(force[1],C)
+    for P in C.platoon:
+        F.add_node(P)
+        color_map.append('yellow')
+        F.add_edge(C,P)
+        for S in P.section:
+            F.add_node(S)
+            color_map.append('green')
+            F.add_edge(P,S)
+
+# Draw graph of friendly assets
+plt.figure()
+plt.title('Friendly Assets')
+nx.draw(F, node_color=color_map)
+plt.show()
 
 # Plot asset locations
 print('Plotting asset locations...')
+X, Y = np.meshgrid(np.arange(0, env.nX), np.arange(0, env.nY))
+Z = env.getTerrainCellElevations()
 plt.figure()
-plt.contourf(env.X, env.Y, env.Z)
-x = []
-y = []
-for C in np.arange(0, len(adversary.company)):
-    for P in np.arange(0, len(adversary.company[C].platoon)):
-        for S in np.arange(0, len(adversary.company[C].platoon[P].section)):
-            x.append(adversary.company[C].platoon[P].section[S].location[0])
-            y.append(adversary.company[C].platoon[P].section[S].location[1])
-plt.scatter(x, y, c='r', marker='.')
-x = []
-y = []
-for C in np.arange(0, len(friendly.company)):
-    for P in np.arange(0, len(friendly.company[C].platoon)):
-        for S in np.arange(0, len(friendly.company[C].platoon[P].section)):
-            x.append(friendly.company[C].platoon[P].section[S].location[0])
-            y.append(friendly.company[C].platoon[P].section[S].location[1])
-plt.scatter(x, y, c='b', marker='.')
+plt.contourf(X, Y, Z)
+c = ['r', 'b']
+a = 0
+for F in force:
+    x = []
+    y = []
+    for C in np.arange(0, len(F.company)):
+        for P in np.arange(0, len(F.company[C].platoon)):
+            for S in np.arange(0, len(F.company[C].platoon[P].section)):
+                for M in np.arange(0, len(F.company[C].platoon[P].section[S].unit.member)):
+                    x.append(F.company[C].platoon[P].section[S].unit.member[M].location[0])
+                    y.append(F.company[C].platoon[P].section[S].unit.member[M].location[1])
+    plt.scatter(x, y, c=c[a], marker='.')
+    a += 1
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title('Asset Location')
 plt.show()
+
+# Develop general course of action
+#for F in force:
+#    F.decomposeObjective(F.objective, env)
+#    print('Number of objectives: %s' % nx.number_of_nodes(F.obj_graph))
+#    print('Number of dependecies: %s' % nx.number_of_edges(F.obj_graph))
+
+# Assign assets to objectives
+#for F in force:
+#    F.assignAssetObjectives()
+
+# Step through time
+for t in np.arange(0, 50):
+    print('Timestep: %s' % t)
+    # Perform timestep operations
+    timestep()
+    # Plot asset locations
+    print('Plotting asset locations...')
+    X, Y = np.meshgrid(np.arange(0, env.nX), np.arange(0, env.nY))
+    Z = env.getTerrainCellElevations()
+    plt.figure()
+    plt.contourf(X, Y, Z)
+    c = ['r', 'b']
+    a = 0
+    for F in force:
+        x = []
+        y = []
+        for C in np.arange(0, len(F.company)):
+            for P in np.arange(0, len(F.company[C].platoon)):
+                for S in np.arange(0, len(F.company[C].platoon[P].section)):
+                    for M in np.arange(0, len(F.company[C].platoon[P].section[S].unit.member)):
+                        x.append(F.company[C].platoon[P].section[S].unit.member[M].location[0])
+                        y.append(F.company[C].platoon[P].section[S].unit.member[M].location[1])
+        plt.scatter(x, y, c=c[a], marker='.')
+        a += 1
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Asset Location')
+    plt.show()
