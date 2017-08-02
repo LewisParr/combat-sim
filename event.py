906c312df204c4d7c81abd5b57c85064648992fc
Lewis C. Parr <lewisparr@gmail.com>
2017-08-02 23:42:09 +0100

    Fire Events
    Added fire events.

--- event.py
+++ event.py
@@ -10,7 +10,7 @@ class Event(object):
     """
     pass
 
-class MovementEvent(object):
+class MovementEvent(Event):
     """
     ...
     """
@@ -25,3 +25,25 @@ class MovementEvent(object):
     def apply(self, force):
         for i in np.arange(0, len(force[self.source[0]].company[self.source[1]].platoon[self.source[2]].section[self.source[3]].unit.member[self.source[4]].location)):
             force[self.source[0]].company[self.source[1]].platoon[self.source[2]].section[self.source[3]].unit.member[self.source[4]].location[i] += self.change[i]
+
+class FireEvent(Event):
+    """
+    ...
+    """
+    def __init__(self, source, target, phit):
+        self.type = 'FIRE'
+        self.source = source
+        self.target = target
+        self.phit = phit
+    
+    def calculate(self):
+        random_sample = np.random.uniform(low=0.0, high=1.0)
+        if random_sample < self.phit:
+            self.hit_result = True
+        else:
+            self.hit_result = False
+    
+    def apply(self, force):
+        if self.hit_result == True:
+            force[self.target[0]].company[self.target[1]].platoon[self.target[2]].section[self.target[3]].unit.member[self.target[4]].hit()
+            
