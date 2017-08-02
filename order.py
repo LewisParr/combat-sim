4259ae4edea4243542a0dea5e3e817afd6436736
Lewis C. Parr <lewisparr@gmail.com>
2017-08-02 23:43:13 +0100

    Hold Order
    Added hold order.

--- order.py
+++ order.py
@@ -8,7 +8,7 @@ class Order(object):
     """
     pass
 
-class MoveTo(object):
+class MoveTo(Order):
     """
     MoveTo order is given to relocate a unit when it is not expected to 
     encounter opposition.
@@ -19,3 +19,10 @@ class MoveTo(object):
         """
         self.type = 'MOVETO'
         self.target = target
+
+class Hold(Order):
+    """
+    Hold order is given to have a unit hold its current position.
+    """
+    def __init__(self):
+        self.type = 'HOLD'
