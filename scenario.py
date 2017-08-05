c8fbe118b47f64740631c9cb817b1d50ebc46715
Lewis C. Parr <lewisparr@gmail.com>
2017-08-05 02:40:49 +0100

    FOB Locations
    Added FOB locations.

--- scenario.py
+++ scenario.py
@@ -17,6 +17,8 @@ class TestScenario(Scenario):
     def __init__(self):
         self.adversary_objective = objective.HoldArea([150, 200], [200, 150])
         self.friendly_objective = objective.TakeArea([150, 200], [200, 150])
+        self.adversary_fob_location = [225, 225]
+        self.friendly_fob_location = [25, 25]
         self.adversary_assets = [[2, 2, 2], [2, 2, 2]]
         self.friendly_assets = [[2, 2, 2], [2, 2, 2]]
         self.adversary_asset_locations = [[[[125, 210], [130, 230]], [[130, 200], [125, 205]], [[130, 200], [140, 200]]], [[[150, 190], [155, 175]], [[150, 175], [160, 200]], [[170, 180], [180, 180]]]]
